122f9ce1703ee0200550588d630cb1e8d9904a49
Mokshith S <mokshith.s@fractal.ai>
2026-08-25 16:25:32 +0530

    transformations changed null handling

--- src/transformations.py
+++ src/transformations.py
@@ -6,7 +6,8 @@ from pyspark.sql import functions as F
 
 def clean_data(df: DataFrame) -> DataFrame:
     """Clean transaction data."""
-    df = df.filter(df["amount"] > 0)
+    df = df.na.drop(how='any')
+    df = df.filter(df['amount'].isNotNull())
     return df
 
 
